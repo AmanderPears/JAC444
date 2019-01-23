67efe522a23f30c6365e78f7e0fb630447af038d
Amander Pears <amander.jp@gmail.com>
2019-01-22 23:42:55 -0500

    update

--- WORKSHOP02/src/noUI/Task02.java
+++ WORKSHOP02/src/noUI/Task02.java
@@ -41,19 +41,29 @@ public class Task02 {
 	public Task02(Scanner getInput) {
 
 		System.out.print("Enter the number of rows and columns in the array: ");
-		int row = getInput.nextInt(), column = getInput.nextInt();
+		// int row = getInput.nextInt(), column = getInput.nextInt();
+		int row = (int) getDouble(getInput), column = (int) getDouble(getInput);
 
 		double[][] twoDimArray = new double[row][column];
 
 		System.out.println("Enter the array:");
 		for (int i = 0; i < twoDimArray.length; i++) {
 			for (int j = 0; j < twoDimArray[i].length; j++) {
-				twoDimArray[i][j] = getInput.nextDouble();
+				twoDimArray[i][j] = getDouble(getInput);
 			}
 		}
 
 		System.out.format("The location of the largest element is %s\n", Location.locateLargest(twoDimArray));
 
+	}
+
+	private double getDouble(Scanner in) {
+
+		while (!in.hasNextDouble()) {
+			in.next();
+		}
+
+		return in.nextDouble();
 	}
 
 }
